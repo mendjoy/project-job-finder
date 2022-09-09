ec09ea310677761a46b621255d94054e54051c41
Joyce M Miranda <106771419+mendjoy@users.noreply.github.com>
2022-09-09 17:13:47 -0300

    update

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const app = express();
 const db = require('./db/connection');
+const bodyParser = require('body-parser');
 
 
 const PORT = 7000;
 
 
 app.listen(PORT, function(){
-    console.log('O express está rodando na porta 7000r')
+    console.log('O express está rodando na porta 7000')
 });
+
+//body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+
 //db connection 
 db //promise
     .authenticate()
     .then(() => {
         console.log('Conectou ao banco com sucesso')
-    });
-    .catch(err )=> ({
-        console.log('ocorre um erro ao conectar', err),
     })
+    .catch(err => console.log(err));
 
 //routes
 app.get('/', (req, res) => {
     res.send('Está funcionando ')
 });
+
+//jobs route
+app.use('/Jobs', require('./routes/jobs'))
